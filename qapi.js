a485ae49036ab6fe07ce0182db7e29443005f842
Bushra-789 <105729572+Bushra-789@users.noreply.github.com>
2024-06-02 17:55:33 +0530

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"665c626e3f24780021f77499",
+				"665c626e3f24780021f7749c"
+			]
+		},
+		"665c626e3f24780021f77499": {
+			"id": "665c626e3f24780021f77499",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"665c626e3f24780021f7749c": {
+			"id": "665c626e3f24780021f7749c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
